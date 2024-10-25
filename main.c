ce2973d1c2b41d745e2589b7f389af47c18ae650
TheFallenWarrior <thefallenwarrior666@gmail.com>
2024-10-25 07:53:59 -0300

    Add NES 2.0 input device names

--- main.c
+++ main.c
@@ -61,6 +61,71 @@ const char *const vsSystemTypes[] = {
 	"Raid on the Bungeling Bay"
 };
 
+const char *const inputTypes[] = {
+	"Unspecified",
+	"Standard NES/Famicom controllers",
+	"NES Four Score/Satellite",
+	"Famicom Four Players",
+	"VS System (1P via 0x4016)",
+	"VS System (1P via 0x4017)",
+	"Reserved",
+	"VS Zapper",
+	"Zapper (0x4017)",
+	"Two Zappers",
+	"Bandai Hyper Shot Lightgun",
+	"Power Pad Side A",
+	"Power Pad Side B",
+	"Family Trainer Side A",
+	"Family Trainer Side B",
+	"Arkanoid Vaus Controller (NES)",
+	"Arkanoid Vaus Controller (Famicom)",
+	"Two Vaus Controllers plus Famicom Data Recorder",
+	"Konami Hyper Shot Controller",
+	"Coconuts Pachinko Controller",
+	"Exciting Boxing Punching Bag",
+	"Jissen Mahjong Controller",
+	"Party Tap",
+	"Oeka Kids Tablet",
+	"Sunsoft Barcode Battler",
+	"Miracle Piano Keyboard",
+	"Pokkun Moguraa",
+	"Top Rider",
+	"Double-Fisted",
+	"Famicom 3D System",
+	"Doremikko Keyboard",
+	"R.O.B. Gyro Set",
+	"Famicom Data Recorder",
+	"ASCII Turbo File",
+	"IGS Storage Battle Box",
+	"Family BASIC Keyboard plus Famicom Data Recorder",
+	"Dongda PEC-586 Keyboard",
+	"Bit Corp. Bit-79 Keyboard",
+	"Subor Keyboard",
+	"Subor Keyboard plus mouse (3x8-bit protocol)",
+	"Subor Keyboard plus mouse (24-bit protocol via 0x4016)",
+	"SNES Mouse (0x4017.d0)",
+	"Multicart",
+	"Two SNES controllers replacing the two standard NES controllers",
+	"RacerMate Bicycle",
+	"U-Force",
+	"R.O.B. Stack-Up",
+	"City Patrolman Lightgun",
+	"Sharp C1 Cassette Interface",
+	"Standard Controller with swapped Left-Right/Up-Down/B-A",
+	"Excalibur Sudoku Pad",
+	"ABL Pinball",
+	"Golden Nugget Casino extra buttons",
+	"Unknown famiclone keyboard used by the 'Golden Key' educational cartridge",
+	"Subor Keyboard plus mouse (24-bit protocol via 0x4017)",
+	"Port test controller",
+	"Bandai Multi Game Player Gamepad buttons",
+	"Venom TV Dance Mat",
+	"LG TV Remote Control",
+	"Famicom Network Controller",
+	"King Fishing Controller",
+	"Croaky Karaoke Controller"
+};
+
 const char *const officialPrgSizes[] = {
 	"64",
 	"16",
@@ -240,7 +305,7 @@ void printINesHeaderInfo(){
 
 		printf(" Frame timing: %s\n", regionNames[iNesHeader[12]]);
 		printf(" Misc ROMs: %d\n", iNesHeader[14]);
-		printf(" Input Device: 0x%02x\n\n", iNesHeader[15]);
+		printf(" Input Device: %s\n\n", inputTypes[iNesHeader[15]]);
 	} else printf(" System: %s\n\n", (iNesHeader[7]&0x3) < 3 ? systemNames[iNesHeader[7]&0x3] : "Other");
 }
 
